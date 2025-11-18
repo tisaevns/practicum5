f5b6a219901c59a49ac5e2d9bbfe0648e20ba935
tisaevns <t.isaev@g.nsu.ru>
2025-11-18 12:02:43 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,20 @@
 import math
-
+import turtle
 xc = float(input("xc: "))
 yc = float(input("yc: "))
 r = float(input("r: "))
 x = float(input("x: "))
 y = float(input("y: "))
+
+turtle.penup()
+turtle.goto(xc, yc - r)
+turtle.pendown()
+turtle.circle(r)
+
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.dot(10, 'red')
 
 distance = math.sqrt((x - xc)**2 + (y - yc)**2)
 
